74b0ea3f756e0f7c0dd9fc563b5fb34a259c6159
Anastasia Armstrong <aarmstrong1@mail.weber.edu>
2025-06-16 13:24:10 -0600

    Home page

--- PortfolioDatabase/portfolioDB/views.py
+++ PortfolioDatabase/portfolioDB/views.py
@@ -2,9 +2,16 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Portfolio
 from .models import Hobby
+from django.template import loader
+
 # Create your views here.
 def Home(request):
-    return HttpResponse("Hello, world. You're at the home page. To get to portfolio: /portfolio/  hobbies: /Hobbies/ Contact: /contact/")
+   template = loader.get_template('portfolioDB/home.html')
+   context = {
+       'portfolio': Portfolio.objects.all(),
+   }
+   return render(request, 'portfolioDB/home.html')
+
 def Portfolios(request):
     portfolio = Portfolio.objects.all()
     return HttpResponse(portfolio)
